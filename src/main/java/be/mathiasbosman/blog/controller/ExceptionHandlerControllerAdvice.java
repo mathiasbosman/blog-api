77cd4b5942a43e1eae0fc553cd43defdf055edf4
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-03-23 09:24:53 +0100

    Remove @NonNull for objects

--- src/main/java/be/mathiasbosman/blog/controller/ExceptionHandlerControllerAdvice.java
+++ src/main/java/be/mathiasbosman/blog/controller/ExceptionHandlerControllerAdvice.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.event.Level;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
@@ -43,7 +42,7 @@ public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHan
 
   private ResponseEntity<Map<String, Object>> createResponseEntityAndLogException(
       WebRequest request,
-      HttpStatus httpStatus, Throwable exception, @NonNull Level logLevel) {
+      HttpStatus httpStatus, Throwable exception, Level logLevel) {
     ResponseEntity<Map<String, Object>> response = createResponseEntity(request, httpStatus);
     final String logPl = "Error with ID: {} - {} - {}";
     switch (logLevel) {
@@ -61,13 +60,13 @@ public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHan
     return response;
   }
 
-  private Object errorIdFromErrorAttributes(@NonNull ResponseEntity<Map<String, Object>> response) {
+  private Object errorIdFromErrorAttributes(ResponseEntity<Map<String, Object>> response) {
     return Optional.ofNullable(response.getBody())
         .map(body -> body.get(ErrorAttributesWithUuid.ERROR_ID)).orElse("");
   }
 
   private ResponseEntity<Map<String, Object>> createResponseEntity(WebRequest request,
-      @NonNull HttpStatus httpStatus) {
+      HttpStatus httpStatus) {
     Map<String, Object> attributes = errorAttributes.getErrorAttributes(request,
         ErrorAttributeOptions.of(Include.MESSAGE, Include.EXCEPTION));
     attributes.put("status", httpStatus.value());
